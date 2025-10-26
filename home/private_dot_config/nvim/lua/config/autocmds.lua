9955c1bbce58eaee6826ae9def9dbba24a1665d0
hoonaesh <tarmuid@roslev.net>
2025-10-26 11:45:49 -0700

    feat(neovim): add Claude integration, tweak colorschemes

--- home/private_dot_config/nvim/lua/config/autocmds.lua
+++ home/private_dot_config/nvim/lua/config/autocmds.lua
@@ -6,3 +6,13 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Disable autoformatting for *.fish.tmpl files (gotmpl syntax conflicts with fish formatters)
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = { "fish" },
+  callback = function()
+    if vim.fn.expand("%"):match("%.fish%.tmpl$") then
+      vim.b.autoformat = false
+    end
+  end,
+})
---
+++ home/private_dot_config/nvim/lua/plugins/formatting.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        -- Disable formatting for fish.tmpl files
+        ["fish.tmpl"] = {},
+      },
+    },
+  },
+}
